35c668926d4371208f1ce8fea9b2789b472ec1b5
latif93 <108307359+latif93@users.noreply.github.com>
2023-02-08 16:09:20 -0600

    part 2 version

--- streamer.py
+++ streamer.py
@@ -14,6 +14,8 @@ class Streamer:
         self.dst_ip = dst_ip
         self.dst_port = dst_port
         self.buffer = OrderedDict()
+        self.sequence_num_sent = 0
+        self.curr_packet_recv = 0
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
         # Your code goes here!  The code below should be changed!
@@ -22,25 +24,25 @@ class Streamer:
 
         num_packets = len(data_bytes) / 1440
         for i in range(int(num_packets)):
-        #    packet = pack('i1440s', i, data_bytes[i*1440:(i+1)*1440])
-        #    self.socket.sendto(packet, (self.dst_ip, self.dst_port))
-            self.socket.sendto(data_bytes[i*1440:(i+1)*1440], (self.dst_ip, self.dst_port))
+            packet = pack('i1440s', self.sequence_num_sent, data_bytes[i*1440:(i+1)*1440])
+            self.sequence_num_sent += 1
+            self.socket.sendto(packet, (self.dst_ip, self.dst_port))
         if len(data_bytes) % 1440 != 0:
-        #    packet = pack(f'i1440s', int(num_packets), data_bytes[int(num_packets)*1440::])
-        #    self.socket.sendto(packet, (self.dst_ip, self.dst_port))
-            self.socket.sendto(data_bytes[int(num_packets)*1440::], (self.dst_ip, self.dst_port))
+            packet = pack(f'i1440s', self.sequence_num_sent, data_bytes[int(num_packets)*1440::])
+            self.sequence_num_sent += 1
+            self.socket.sendto(packet, (self.dst_ip, self.dst_port))
         
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
-        # your code goes here!  The code below should be changed!
-        
-        # this sample code just calls the recvfrom method on the LossySocket
-        data, addr = self.socket.recvfrom()
-       # header, data = unpack('i1440s', data) 
-       # last_sequence_num = self.buffer.keys()[-1]
-       # self.buffer[header] = data
-        # For now, I'll just pass the full UDP payload to the app
-        return data.split(b'\x00')[0]
+        while str(self.curr_packet_recv) not in self.buffer.keys():
+            data, addr = self.socket.recvfrom()
+            header, data = unpack('i1440s', data)
+            self.buffer[str(header)] = data.split(b'\x00')[0]
+        data = self.buffer[str(self.curr_packet_recv)]
+        self.buffer.pop(str(self.curr_packet_recv))
+        self.curr_packet_recv +=1
+        return data
+
 
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
